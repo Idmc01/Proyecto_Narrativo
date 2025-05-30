50826fb3153b83b8cf6b8352241f1c221246b385
mauc1 <141196849+mauc1@users.noreply.github.com>
2025-05-29 22:10:45 -0500

    timer
    timeeer

---
+++ Assets/Scripts/stopTimer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class stopTimer : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Asegúrate que el objeto con el temporizador tiene este tag o nombre
+        CountdownTimerTMP temporizador = FindObjectOfType<CountdownTimerTMP>();
+
+        if (temporizador != null)
+        {
+            temporizador.tiempoInicial = 1000f;
+            Destroy(temporizador);
+        }
+    }
+}
